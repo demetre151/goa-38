42a99206769e0fb251cf6dc92a90353f6cb79835
demetre151 <tsetskhladzedemetre5@gmail.com>
2024-09-14 20:05:27 +0300

    commit

--- level 06/classwork/classwork.py
+++ level 06/classwork/classwork.py
@@ -2,4 +2,4 @@ name = "data"
 my_surname_is = "diasamidze"
 age = 100
 surname = 1.50
-print# (#name + my_surname_is + " " + surname + "my age is" + " " + str(age)
+print#(name + my_surname_is + " " + surname + "my age is" + " " + str(age)
